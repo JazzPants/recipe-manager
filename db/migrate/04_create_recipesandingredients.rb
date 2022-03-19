cf7e269e3101978121cd80bb1b899a4e58f7b214
Leighton <leighton.l14@gmail.com>
2022-03-19 13:45:03 +1100

    8th Commit - Migrated models

--- db/migrate/04_create_recipesandingredients.rb
+++
@@ -1,8 +0,0 @@
-class RecipesAndIngredients < ActiveRecord::Migration[4.2]
-    def change
-      create_table :recipes_ingredients do |t|
-        t.belongs_to :recipe
-        t.belongs_to :ingredient
-      end
-    end
-  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 6) do
 
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "rating"
+    t.integer "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "procedure"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_foreign_key "comments", "users"
+  add_foreign_key "ratings", "users"
+  add_foreign_key "recipes", "users"
+  add_foreign_key "recipes_ingredients", "ingredients"
+  add_foreign_key "recipes_ingredients", "recipes"
 end
