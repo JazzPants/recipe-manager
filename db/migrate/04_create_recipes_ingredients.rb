e67f05aef756f6f375db07b563894466180530a5
Leighton <leighton.l14@gmail.com>
2022-03-21 20:08:25 +1100

    11th Commit - Added Link to user recipes, and id:false

--- db/migrate/04_create_recipes_ingredients.rb
+++ db/migrate/04_create_recipes_ingredients.rb
@@ -1,6 +1,6 @@
 class CreateRecipesIngredients < ActiveRecord::Migration[4.2]
     def change
-      create_table :recipes_ingredients do |t|
+      create_table :recipes_ingredients, id: false do |t|
         t.belongs_to :recipe, foreign_key: true
         t.belongs_to :ingredient, foreign_key: true
       end
